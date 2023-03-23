ea90924ab279bee2fc83e9b7ede8e0d05f56517e
KubaGlegola <kubaglegola@gmail.com>
2023-03-23 12:53:53 +0100

    add more items images

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -3,15 +3,26 @@ import Button from '../button/Button';
 import './Item.scss'
 
 const Item = (props) => {
+    const {id, image, categories, name, producer, description} = props;
+
   return (
     <div className="item">
         <div className="item__imageContainer">
-            <img src='/images/items/motopompa.png' className="item__image" />
+            <img src={image} className="item__image" />
         </div>
         <div className="item__content">
-            <span className="item__category">ogród</span>
-            <h3 className="item__name">motopompa</h3>
-            <p className="item__producer">cedrus</p>
+            <div className="item__categories">
+                {categories.map(category=>{
+                    return <span className={`item__category ${
+                        category === "ogród" ? "item__category--garden" : 
+                        category === "dom" ? "item__category--house" : 
+                        category === "budowa" ? "item__category--construction" : 
+                        category === "narzędzia" ? "item__category--tools" :
+                        ""}`}>{category}</span>
+                })}
+            </div>            
+            <h3 className="item__name">{name}</h3>
+            <p className="item__producer">{producer}</p>
             <p className="item__description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro harum corrupti, suscipit doloremque voluptas, facere in quidem accusamus velit est a maxime? Itaque dolorem minima quam dolore voluptatum, magni praesentium?</p>
             <Button className="item__button button--primary">Pokaż szczegóły</Button>
         </div>
--- src/components/Items/Items.jsx
+++ src/components/Items/Items.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Item from '../Item/Item'
 import './Items.scss'
+import items from '../../assets/data/items';
 
 const Items = () => {
   return (
     <div className="items">
-        <Item/>
-        <Item/>
+        {items.map((item) => {
+            return <Item key={item.id} id={item.id} image={item.img} categories={item.category} name={item.name} producer={item.producer} description={item.description} /> 
+        })}
     </div>
   )
 }
